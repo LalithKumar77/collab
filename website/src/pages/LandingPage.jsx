4f7d567344c4706b28dd8279f5570a9a04c98a67
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-10-20 03:15:57 +0530

    dhanu

--- website/src/pages/LandingPage.jsx
+++ website/src/pages/LandingPage.jsx
@@ -2,47 +2,88 @@ import "../styles/LandingPage.css";
 import { Link } from 'react-router-dom';
 import video from "../assets/LandingPage/background_animation.mp4";
 import UserCard from "../components/LandingPage/user_card";
+import pic1 from "../assets/LandingPage/me1.jpg";
 
-
-function HomePage(){
-    return(
+function HomePage() {
+    return (
         <div className="lp">
-        <div className="lp_main">
-            <div className="lp_head">
-                <div className="lp_logo"><img alt="logo" src=""></img></div>
-                <div className="lp_options">
-                    <ul>
-                    <Link to="/"><li>Home</li></Link>
-                        <Link to="/contact-us"><li>Contact Us</li></Link>
-                        <Link to="/login"><li>Register</li></Link>
-                        <Link to="/register"><li>Login</li></Link>
-                    </ul>
+            <div className="lp_main">
+                <div className="lp_head">
+                    <div className="lp_logo"><img alt="logo" src=""></img></div>
+                    <div className="lp_options">
+                        <ul>
+                            <Link to="/"><li>Home</li></Link>
+                            <Link to="/contact-us"><li>Contact Us</li></Link>
+                            <Link to="/login"><li>Register</li></Link>
+                            <Link to="/register"><li>Login</li></Link>
+                        </ul>
+                    </div>
+                </div>
+                <div className="lp_body">
+                    <video autoPlay muted loop>
+                        <source src={video} type="video/mp4" />
+                    </video>
+                    <div className="lp_title">
+                        <h1>Your Voice, Your Ideas, Their Innovation: A Platform for Raising Issues and Sharing Ideas</h1>
+                        <h5>Connect with companies. Get your issues heard. Share ideas. Earn rewards.</h5>
+                    </div>
                 </div>
             </div>
-            <div className="lp_body">
-                <video autoPlay muted loop>
-                    <source src={video} type="video/mp4" />
-                </video>
-                <div className="lp_title">
-                    <h1>Your Voice, Your Ideas, Their Innovation: A Platform for Raising Issues and Sharing Ideas</h1>
-                    <h5>Connect with companies. Get your issues heard. Share ideas. Earn rewards.</h5>
+            <div className="lp_users">
+                <div className="lp_user_dash">
+                    <h1>Our Users are Changing the Game</h1>
+                    <div className="lp_cards">
+                        <div className="lp_card_all">
+                            {/* First set of user cards */}
+                            <div className="lp_user_card">
+                                <UserCard name="Dhanush" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" im={pic1} />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Chandu" des="I submitted an idea, and it turned into a patent! A company even bid for it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="Why are u gay" />
+                            </div>
+                        </div>
+                        {/* Duplicate the card set for seamless scrolling effect */}
+                        <div className="lp_card_all">
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" im={pic1} />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Chandu" des="I submitted an idea, and it turned into a patent! A company even bid for it!" />
+                            </div>
+                            <div className="lp_user_card">
+                                <UserCard name="Lalith" des="Why are u gay" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="lp_users">
-            <div className="lp_user_dash">
-                <h1>Our Users are Changing the Game</h1>
-                <div className="lp_cards">
-                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
-                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
-                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
-                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
-                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
-                    <UserCard name="Chandu" des="I submitted an idea, and it turned into a patent! A company even bid for it!" />
-                    <UserCard name="Lalith" des="Why are u gay" />
-                </div>
-            </div>
-        </div>
         </div>
     );
 }
