2abb93c8ec9e9ff1cbeb2cfe21e95ce3e2fffe37
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-10-20 01:17:00 +0530

    hi

--- website/src/components/LandingPage/user_card.jsx
+++ website/src/components/LandingPage/user_card.jsx
@@ -1,13 +1,26 @@
 import "./user_card.css";
-function UserCard(props){
-    return(
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+function UserCard(props) {
+    return (
         <div className="user_card">
             <div className="uc_top">
-                <img alt="user" src={props.im}></img>
+                <img alt={`Profile of ${props.name}`} src={props.im} />
             </div>
             <div className="uc_content">
                 <h1>{props.name}</h1>
                 <h3>{props.des}</h3>
+                <button className="uc_button">View Profile</button> {/* New Button */}
+            </div>
+            <div className="uc_contact">
+                <a href="https://www.instagram.com/dhanush_awesome7" aria-label="Instagram">
+                    <FontAwesomeIcon icon={faInstagram} className="icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/dhanush-awesome" aria-label="LinkedIn">
+                    <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                </a>
+                {/* You can add more social links as needed */}
             </div>
         </div>
     );
--- website/src/pages/LandingPage.jsx
+++ website/src/pages/LandingPage.jsx
@@ -6,11 +6,11 @@ import UserCard from "../components/LandingPage/user_card";
 
 function HomePage(){
     return(
-        <div className="hp">
-        <div className="hp_main">
-            <div className="hp_head">
-                <div className="hp_logo"><img alt="logo" src=""></img></div>
-                <div className="hp_options">
+        <div className="lp">
+        <div className="lp_main">
+            <div className="lp_head">
+                <div className="lp_logo"><img alt="logo" src=""></img></div>
+                <div className="lp_options">
                     <ul>
                     <Link to="/"><li>Home</li></Link>
                         <Link to="/contact-us"><li>Contact Us</li></Link>
@@ -19,23 +19,26 @@ function HomePage(){
                     </ul>
                 </div>
             </div>
-            <div className="hp_body">
+            <div className="lp_body">
                 <video autoPlay muted loop>
                     <source src={video} type="video/mp4" />
                 </video>
-                <div className="hp_title">
+                <div className="lp_title">
                     <h1>Your Voice, Your Ideas, Their Innovation: A Platform for Raising Issues and Sharing Ideas</h1>
                     <h5>Connect with companies. Get your issues heard. Share ideas. Earn rewards.</h5>
                 </div>
             </div>
         </div>
-        <div className="hp_users">
-            <h1>Our Users are Changing the Game</h1>
-            <div className="hp_user_dash">
+        <div className="lp_users">
+            <div className="lp_user_dash">
                 <h1>Our Users are Changing the Game</h1>
-                <div className="hp_cards">
-                    <UserCard name="Lalith" des="Why are u gay" />
-                    <UserCard name="Lalith" des="Why are u gay" />
+                <div className="lp_cards">
+                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                    <UserCard name="Lalith" des="I raised an issue with a popular app, and within a week, they responded and fixed it!" />
+                    <UserCard name="Chandu" des="I submitted an idea, and it turned into a patent! A company even bid for it!" />
                     <UserCard name="Lalith" des="Why are u gay" />
                 </div>
             </div>
