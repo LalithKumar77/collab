47b9bb47fa5d486bf58879d0066ba74c950695b4
Lalith Kumar <allulalithkumar77@gmail.com>
2024-10-21 21:55:24 +0530

    db schema updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`Database Connected: ${process.env.MONGO_URI}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database Connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
